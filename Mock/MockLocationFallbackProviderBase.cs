1d1e00494dcf2610506fe08fd0456838c85ce61a
Artsem Prashkovich <a.prashkovich@brimit.com>
2018-06-05 15:45:48 +0300

    Repository is restructured. Module package was added to the repository and it is available to download.

--- Mock/MockLocationFallbackProviderBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Configuration.Provider;
-
-namespace GeoIpFallback.Mock
-{
-    public abstract class MockLocationFallbackProviderBase : ProviderBase
-    {
-        public abstract Sitecore.Analytics.Model.WhoIsInformation GetMockCurrentLocation();
-    }
-}
